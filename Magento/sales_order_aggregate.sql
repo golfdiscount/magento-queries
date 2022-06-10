e009786db32fa6c18284ecad031aa4b9f87b645f
Harmeet Singh <harmeet@golfdiscount.com>
2022-06-10 14:47:34 -0700

    Updated date range to exclude current date

--- Magento/sales_order_aggregate.sql
+++ Magento/sales_order_aggregate.sql
@@ -1,9 +1,9 @@
-# Aggregate of sales throughout the past week
 SELECT DATE(created_at) AS "Date",
 	COUNT(*) AS "Count",
     FORMAT(SUM(grand_total), 2) AS "Grand Total"
 FROM sales_order
 WHERE increment_id LIKE "100%"
-AND DATE(created_at) > SUBDATE(CURDATE(), INTERVAL 1 WEEK)
+	AND DATE(created_at) >= SUBDATE(CURDATE(), INTERVAL 1 WEEK)
+	AND DATE(created_at) < DATE(CURDATE())
 GROUP BY DATE(created_at)
 ORDER BY DATE(created_at) DESC;
--- sales_order_aggregate.sql
+++ sales_order_aggregate.sql
@@ -3,6 +3,7 @@ SELECT DATE(created_at) AS "Date",
 	COUNT(*) AS "Count",
     FORMAT(SUM(grand_total), 2) AS "Grand Total"
 FROM sales_order
-WHERE DATE(created_at) > SUBDATE(CURDATE(), INTERVAL 1 WEEK)
+WHERE DATE(created_at) >= SUBDATE(CURDATE(), INTERVAL 1 WEEK)
+	AND DATE(created_at) < DATE(CURDATE())
 GROUP BY DATE(created_at)
 ORDER BY DATE(created_at) DESC;
